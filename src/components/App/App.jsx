efa501592aaf69ae9dc8209c7b1181a0808e48b3
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-05-15 23:54:31 +0300

    step-2 done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,56 @@
-import Description from '../Description/Description'
-import './App.css'
+import Description from '../Description/Description';
+import './App.css';
+
+import { useState } from 'react';
 
 function App() {
-  
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const updateFeedback = feedbackType => {
+    setFeedback(feedback.feedbackType + 1);
+  };
+
+  const resetFeedback = feedbackType => {
+    // Тут використовуй сеттер, щоб оновити стан
+  };
+
   return (
     <>
-     <Description/> 
+      <Description />
+      <button
+        onClick={updateFeedback(() => {
+          good;
+        })}
+      >
+        Good
+      </button>
+      <button
+        onClick={updateFeedback(() => {
+          neutral;
+        })}
+      >
+        Neutral
+      </button>
+      <button
+        onClick={updateFeedback(() => {
+          bad;
+        })}
+      >
+        Bad
+      </button>
+      <button onClick={resetFeedback}>Reset</button>
+
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total:</p>
+      <p>Positive:</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
